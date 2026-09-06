7d37a89c77eb7fb2a37f0c9779d50018dde16d86
harunkkhan <harunkkhan1@gmail.com>
2026-09-06 16:59:04 -0400

    Export ui package entrypoint

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,21 @@
-export const PACKAGE_NAME = "@patriothacks/ui";
+export { cn } from "cn";
+
+export { AppShell, type AppShellProps } from "#components/app-shell";
+export { Button, buttonVariants } from "#components/button";
+export {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "#components/card";
+export { Input } from "#components/input";
+export { Label } from "#components/label";
+export {
+  ThemeProvider,
+  type ThemeProviderProps,
+  useTheme,
+} from "#components/theme-provider";
+export { ThemeToggle } from "#components/theme-toggle";
